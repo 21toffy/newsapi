451abe905391253fa938f6dc480bb5a6f168095a
21toffy <oketofoke@gmail.com>
2020-07-24 04:41:21 +0100

    Initial commit

--- newsapi/wsgi.py
+++ newsapi/wsgi.py
@@ -1,16 +1,20 @@
-"""
-WSGI config for newsapi project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
+from django.core.wsgi import get_wsgi_application
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+
+
+from whitenoise import WhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsapi.settings')
 
 application = get_wsgi_application()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+application = WhiteNoise(application, root=os.path.join(BASE_DIR,'/static'))
+
+# application = DjangoWhiteNoise(application)
